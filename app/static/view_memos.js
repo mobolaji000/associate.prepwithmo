06126edbd3787f77105f3dfe114b94110bda9ad5
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2021-12-25 11:05:10 -0600

    diffrentiate between tutor and admin viewing reports

--- app/static/view_memos.js
+++ app/static/view_memos.js
@@ -5,6 +5,27 @@ $(document).ready(function() {
 var today = new Date(new Date().getTime() + offset * 3600 * 1000).toISOString().split('T')[0];
         document.getElementById("start_date").value = today;
         document.getElementById("end_date").value = today;
+
+        var is_admin = $("#is_admin");
+            var is_admin = $("#is_admin").attr("data-is_admin");
+            //alert(is_admin);
+            if (is_admin == "False")
+            {
+              var tutor_or_student_list = $("#tutor_or_student_list");
+            var select_options = "";
+            var students_info = $("#students_info").attr("data-students_info");
+            var students_info = JSON.parse(students_info);
+             select_options +=  '<option value="" selected disabled hidden>Choose student</option>';
+             for (var key in students_info){
+                    console.log( key, students_info[key] );
+                    select_options += '<option value="'+key+'">'+students_info[key]+'</option>';
+                }
+                document.getElementById("label_for_tutor_or_student_list").innerHTML = 'Select Student';
+                 document.getElementById("error_for_tutor_or_student_list").innerHTML = 'Valid student is required.';
+                tutor_or_student_list.append(select_options);
+
+            }
+
         };
 
  $('input[name="search_by_tutor_or_student"]').change(function() {
