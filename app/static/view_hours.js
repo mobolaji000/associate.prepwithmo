4858ac3274e994b4d4353766294d594c812bc2b9
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2021-12-25 09:14:35 -0600

    check for admin roles

--- app/static/view_hours.js
+++ app/static/view_hours.js
@@ -12,8 +12,8 @@ var date_to_process = new Date();
 	  while (n < 13) {
 	  var last_month_name = date_to_process.toLocaleString('default', { month: 'long' });
          var last_year_name = date_to_process.getFullYear();
-         console.log(last_month_name+"-"+last_year_name);
-         console.log(" ");
+         //console.log(last_month_name+"-"+last_year_name);
+         //console.log(" ");
 
          var number_of_days_in_last_month = new Date(date_to_process.getFullYear(), date_to_process.getMonth(), 0).getDate();
          date_to_process.setDate(date_to_process.getDate()-number_of_days_in_last_month);
