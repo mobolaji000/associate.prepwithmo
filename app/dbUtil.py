aae5cbf1a7b4f53eb1f03d36fa6f26a73ebea844
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2022-07-05 16:56:35 -0500

    show students and tutors for assignment in ascending order2

--- app/dbUtil.py
+++ app/dbUtil.py
@@ -85,7 +85,7 @@ class AppDBUtil():
 
     @classmethod
     def getTutorStudentsAssignment(cls, tutors_emails=[]):
-        tutor_student_assignments = TutorStudentAssignment.query.filter((TutorStudentAssignment.tutor_email.in_(tutors_emails))).all()
+        tutor_student_assignments = TutorStudentAssignment.query.filter((TutorStudentAssignment.tutor_email.in_(tutors_emails))).order_by(TutorStudentAssignment.tutor_first_name.asc(),TutorStudentAssignment.student_first_name.asc()).all()
         return tutor_student_assignments
 
 
