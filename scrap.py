ce2ba055cc7580f59b177f919270540106a78a63
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2023-02-28 14:02:55 -0600

    testing update requirements and pin sqlalchemy

---
+++ scrap.py
@@ -0,0 +1,26 @@
+
+
+requirements_venv_file = open('requirements_venv.txt', 'r')
+requirements_venv_list = requirements_venv_file.readlines()
+requirements_venv_file.close()
+
+requirements_file = open('requirements.txt', 'r')
+requirements_list = requirements_file.readlines()
+requirements_file.close()
+
+merged_requirements_file = open('merged_requirements.txt', 'a')
+# merged_requirements_list = merged_requirements_file.readlines()
+# merged_requirements_file.close()
+
+found = False
+for requirements_line in requirements_list:
+    for requirements_venv_line in requirements_venv_list:
+        if requirements_venv_line.startswith(requirements_line.split('==')[0]):
+            merged_requirements_file.write(requirements_venv_line)
+            found = True
+            break
+    if not found:
+        merged_requirements_file.write(requirements_line)
+    found = False
+
+
