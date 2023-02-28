29cdbc00ae7cc5a0dc82bfa1dc04a0acc2245ec4
mobolaji00 <mobolaji.akinpelu@yahoo.com>
2023-02-28 14:13:16 -0600

    testing update requirements and pin sqlalchemy2

--- scrap.py
+++ scrap.py
@@ -15,7 +15,7 @@ merged_requirements_file = open('merged_requirements.txt', 'a')
 found = False
 for requirements_line in requirements_list:
     for requirements_venv_line in requirements_venv_list:
-        if requirements_venv_line.startswith(requirements_line.split('==')[0]):
+        if requirements_venv_line.lower().startswith(requirements_line.lower().split('==')[0]):
             merged_requirements_file.write(requirements_venv_line)
             found = True
             break
